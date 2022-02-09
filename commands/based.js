7d9eee85a1db277a6605d65f7e08af96dcd41d81
Merecski <mikemerecki@gmail.com>
2022-02-08 19:57:04 -0500

    Updated based module to print embedded tables to chat

--- commands/based.js
+++ commands/based.js
@@ -1,6 +1,8 @@
-import { SlashCommandBuilder } from '@discordjs/builders'
+import { MessageEmbed } from 'discord.js'
+import { SlashCommandBuilder, codeBlock } from '@discordjs/builders'
 import { checkPartial } from '#utils/utils'
 import { config } from '#utils/config'
+import { table } from 'table'
 
 
 class BasedCounter {
@@ -21,13 +23,20 @@ class BasedCounter {
         	const { commandName } = interaction;
             if (commandName === 'basedcount') {
                 interaction.reply({content: `Based counter: ${this.count(interaction.user.id)}`, ephemeral: true})
+            } else if (commandName === 'basedboard') {
+                const rows = interaction.options.getInteger('rows')
+                let userReq = interaction.options.getUser('user')
+                userReq = userReq ? userReq : interaction.user
+                this.getUsernames(userReq, rows).then(table => {
+                    interaction.reply({content: codeBlock(table), ephemeral: true})
+                })
             }
         })
-    
+
          this.client.on('messageReactionAdd', async (reaction, user) => {
             if (user.bot || await checkPartial(reaction)) return
             if (config.debug && reaction.message.channel.name !== 'bot-testing') return
-            // if (!config.debug && user.author === reaction.author) return
+            if (!config.debug && user.author === reaction.author) return
             this.handleBasedReactions(reaction)
         });
 
@@ -50,16 +59,71 @@ class BasedCounter {
         })
     }
 
+    async getUsernames(requester, max) {
+        let output = []
+        let tableConfig = {}
+        let finalOutput = [["Username", "Count", "You"]]
+        const minCount = finalOutput.length
+        const absoluteMax = 100 // Absolute max number of rows displayed
+        const defaultMax = 10 // Absolute max number of rows displayed
+        const maxLimit = max ? Math.min(Math.max(1, max), absoluteMax) : defaultMax // If requested max is
+        const youPointer = '<---'
+        for (const [id, value] of Object.entries(this.based)) {
+            try {
+                var user = await this.client.users.fetch(id)
+            } catch (error) {
+                console.log(`Skipping userid ${id} (Probably a dev id?)`)
+                continue
+            }
+            const you = id === requester.id ? youPointer : ''
+            output.push([user.username, value, you])
+        }
+        output.sort((a, b) => {
+            return b[1] - a[1]
+        })
+
+        let title = `BASED COUNTER`
+
+        if (output.length > maxLimit) {
+            // Centering table on current user
+            const centerYouIndex = output.findIndex(row => row[2] === youPointer)
+            const beforePre = centerYouIndex - Math.floor(maxLimit / 2)
+            const afterPre = centerYouIndex + Math.ceil(maxLimit / 2)
+            const before = Math.max(0, beforePre - Math.min(0, output.length - afterPre))
+            const after = Math.min(output.length, afterPre - Math.min(0, beforePre))
+            console.log(`${maxLimit}, ${centerYouIndex}, beforePre ${beforePre}, afterPre ${afterPre}, after: ${after}`)
+
+            tableConfig['spanningCells'] = []
+            if (before > 0) {
+                finalOutput.push([`${before} more user${before === 1 ? '' : 's'} ahead...`, '', ''])
+                tableConfig['spanningCells'].push({ col: 0, row: 1, colSpan: 3})
+            }
+            if (after - before > 0) finalOutput = finalOutput.concat(output.slice(before, after))
+            if (output.length > after) {
+                finalOutput.push([`${output.length - after} more user${output.length - after === 1 ? '' : 's'} remaing...`, '', ''])
+                tableConfig['spanningCells'].push({ col: 0, row: finalOutput.length - 1, colSpan: 3})
+            }
+
+            title = title + `\nDisplaying ${maxLimit} rows`
+        } else {
+            finalOutput = output
+        }
+        tableConfig['header'] = { content: title }
+        console.log(finalOutput)
+        return table(finalOutput, tableConfig)
+    }
+
     // Update database for specific user
     update(id) {
         console.log(`Updating user ${id} with count ${this.count(id)}`)
-        this.pool.query(`UPDATE based SET count = ${this.count(id)} WHERE userid = '${id}'`, (err, result, fields) => {
+        this.pool.query(`UPDATE users SET based = ${this.count(id)} WHERE userid = '${id}'`, (err, result, fields) => {
             if (err) throw err
         })
     }
 
     add(msg) {
-        let id = msg.author.id;
+        let id = config.debug ? '474747' : msg.author.id;
+        let username = msg.author.username;
         if (typeof id !== "string") return;
         if (id in this.based) {
             this.based[id] += 1;
@@ -68,7 +132,7 @@ class BasedCounter {
                 if (err) throw err
             })
             this.based[id] = 1;
-            msg.reply(`Congratulations ${msg.author.username} your first based content!`);
+            msg.reply(`Congratulations ${username} your first based content!`);
         }
         if (this.based[id] && this.based[id] % 100 === 0) {
             msg.reply(`Your based counter has reached ${this.based[id]}`);
@@ -85,8 +149,6 @@ class BasedCounter {
     }
 
     count(id) {
-        console.log('Getting count for', id)
-        console.log(this.based)
         return this.based[id] ? this.based[id] : 0;
     }
 
@@ -95,6 +157,7 @@ class BasedCounter {
             const repliedTo = await msg.channel.messages.fetch(msg.reference.messageId)
             this.add(repliedTo);
             repliedTo.react(this.client.emojis.cache.find(emoji => emoji.name === 'based'))
+            console.log(`${msg.author.username} gave ${repliedTo.author.username} +1 based`)
         }
     }
 
@@ -108,6 +171,10 @@ class BasedCounter {
 
 const commands = [
     new SlashCommandBuilder().setName('basedcount').setDescription('Replies with user total based count.'),
+    new SlashCommandBuilder().setName('basedboard').setDescription('Repleis with the complete scoreboard')
+        .addIntegerOption(option => option.setName('rows').setDescription('Max number of rows to print'))
+        .addUserOption(option => option.setName('user').setDescription('Center on specific user')),
+    new SlashCommandBuilder().setName('basedrank').setDescription('Repleis with the complete scoreboard'),
 ]
 
 export {
